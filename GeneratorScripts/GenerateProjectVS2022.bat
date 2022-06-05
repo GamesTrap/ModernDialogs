e210d4e9d7305329bf7ac6bfa3a770b052e45a34
GamesTrap <GamesTrap@web.de>
2022-06-05 04:15:02 +0200

    Updated Premake, VS 2022 support

---
+++ GeneratorScripts/GenerateProjectVS2022.bat
@@ -0,0 +1,7 @@
+@echo off
+pushd %~dp0
+call ..\libs\premake5\windows\premake5.exe --file=../premake5.lua vs2022
+IF %ERRORLEVEL% NEQ 0 (
+  PAUSE
+)
+popd
---
+++ GeneratorScripts/GenerateProjectVS2022.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+exec="../libs/premake5/linux/./premake5 --file=../premake5.lua vs2022"
+$exec
+if [[ "$?" -ne 0 ]]; then
+    read -s -N 1 -p "Press any key to continue...";
+fi
+
