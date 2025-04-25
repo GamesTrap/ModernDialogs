9226f5ffe8a2a3af954ddcc2792fe43d0dc735e3
GamesTrap <GamesTrap@web.de>
2025-04-25 20:21:34 +0200

    Updated license

--- ModernDialogs/ModernDialogs.cpp
+++ ModernDialogs/ModernDialogs.cpp
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2020 - 2023 Jan "GamesTrap" Schürkamp
+Copyright (c) 2020 - 2025 Jan "GamesTrap" Schürkamp
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
